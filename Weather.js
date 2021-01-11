f3d72db70079ed68dc027782dda3fbdb073628f0
rooopy96 <57246751+rooopy96@users.noreply.github.com>
2021-01-11 23:21:43 +0900

    Conclusions

--- Weather.js
+++ Weather.js
@@ -7,19 +7,27 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 const weatherOptions = {
 	Thunderstorm: {
 		iconName: "weather-lightning",
-    gradient: ["#373B44", "#4286f4"]
+		gradient: ["#373B44", "#4286f4"],
+		title: "Thunderstorm in the house",
+    subtitle: "Actually, outside of the house"
 	},
 	Drizzle: {
 		iconName: "weather-hail",
-    gradient: ["#89F7FE", "#66A6FF"]
+		gradient: ["#89F7FE", "#66A6FF"],
+		title: "Drizzle",
+    subtitle: "Is like rain, but gay 🏳️‍🌈"
 	},
   Rain: {
 		iconName: "weather-rainy",
-    gradient: ["#00C6FB", "#005BEA"]
+		gradient: ["#00C6FB", "#005BEA"],
+		title: "Raining like a MF",
+    subtitle: "For more info look outside"
 	},
   Snow: {
 		iconName: "weather-snowy",
-    gradient: ["#7DE2FC", "#B9B6E5"]
+		gradient: ["#7DE2FC", "#B9B6E5"],
+		title: "Cold as balls",
+    subtitle: "Do you want to build a snowman? Fuck no."
 	},
   Atmosphere: {
 		iconName: "weather-hail",
@@ -27,11 +35,15 @@ const weatherOptions = {
 	},
   Clear: {
 		iconName: "weather-sunny",
-    gradient: ["#FF7300", "#FEF253"]
+		gradient: ["#FF7300", "#FEF253"],
+		title: "Sunny as fuck",
+    subtitle: "Go get your ass burnt"
 	},
   Clouds: {
 		iconName: "weather-cloudy",
-    gradient: ["#D7D2CC", "#304352"]
+		gradient: ["#D7D2CC", "#304352"],
+		title: "Clouds",
+    subtitle: "I know, fucking boring"
 	},
   Haze: {
 		iconName: "weather-hail",
@@ -41,11 +53,15 @@ const weatherOptions = {
 	},
   Mist: {
 		iconName: "weather-hail",
-    gradient: ["#4DA0B0", "#D39D38"]
+		gradient: ["#4DA0B0", "#D39D38"],
+		title: "Mist!",
+    subtitle: "It's like you have no glasses on."
 	},
   Dust: {
 		iconName: "weather-hail",
-    gradient: ["#4DA0B0", "#D39D38"]
+		gradient: ["#4DA0B0", "#D39D38"],
+		title: "Dusty",
+    subtitle: "Thanks a lot China 🖕🏻"
 	}
 }
 
